8883a308b7d0f881fd3681be1056b592e4cb396c
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-21 00:39:02 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :employees
       t.boolean :in_business
       t.datetime :opening_time
-      t.datetiem :closing_time
+      t.datetime :closing_time
     end
   end
 end
